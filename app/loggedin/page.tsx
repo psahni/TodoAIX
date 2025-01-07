e3bb16215390ede17a63da596edab2ae0749aa74
prashant.sahni <prashant.sahni@codeandtheory.com>
2025-01-07 14:51:56 +0530

    fetch google image

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,6 +1,12 @@
 
 import Tasks from "@/components/todoaix/tasks";
+import UserProfile from "@/components/todoaix/user-profile";
 
 export default function Page() {
-  return <Tasks />;
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <UserProfile />
+      <Tasks />
+    </main>
+  );
 }
---
+++ components/todoaix/user-profile.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useSession } from "next-auth/react"
+import Image from "next/image"
+import React from "react"
+
+export default function UserProfile() {
+  const session = useSession();
+  if (!session) {
+    return null;
+  }
+  return (
+    <div>
+      <Image
+        src={session?.data?.user?.image || "/default-profile.png"}
+        width={24}
+        height={24}
+        alt="user profile"
+        className="rounded-full"
+      />
+  </div>)
+}
